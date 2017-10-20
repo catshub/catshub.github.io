d25d5f1bf6d11071898173479a1edbbe84434ef9
XQ <xiongqi.me@foxmail.com>
2017-10-20 20:20:13 +0800

    背景图渲染逻辑优化 2017-10-20 20:20

--- resources/sort.js
+++ resources/sort.js
@@ -1,5 +1,4 @@
 function quickSort(arr, left, right) {
-
     //left < right , 继续排序
     if (left < right) {
         var i = left, j = right + 1;//j=right+1,用于--j
@@ -16,6 +15,7 @@ function quickSort(arr, left, right) {
         }
         arr[left] = arr[j];
         arr[j] = key;
+        var tt=arr.slice(0,j),t2=arr.slice(j+1);
         quickSort(arr, left, j - 1);
         quickSort(arr, j + 1, right);
     }
@@ -40,3 +40,31 @@ rl.on("line", function (data) {
     quickSort(arr2, 0, arr2.length - 1);//string
     console.log(arr+"\n"+arr2);
 })
+
+
+var count=0;
+function quickSort2(arr) {
+    //left < right , 继续排序
+    var left=0,right=arr.length-1;
+    if (left < right) {
+        var i = left, j = right + 1;//j=right+1,用于--j
+        var key = arr[left];
+        var temp;
+        while (true) {
+            while (arr[--j] > key);
+            while (arr[++i] < key && i < right);
+            if (i >= j) break;
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+        arr[left] = arr[j];
+        arr[j] = key;
+        var tt=arr.slice(0,j),t2=arr.slice(j+1);
+        console.log(tt,arr[j],t2);
+        quickSort2(tt);
+        quickSort2(t2);
+        // arr=tt.concat(arr[j]).concat(t2);
+        arr.splice(0,tt.length,...tt);
+        arr.splice(j+1,t2.length,...t2);
+        // for(var m=0;m
